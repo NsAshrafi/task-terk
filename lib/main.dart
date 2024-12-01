272d4570abad24ca4ce3f53687cd4ca5804345df
Nafiseh Ashrafi <nafisehashrafi@192.168.1.65>
2024-12-01 15:56:02 +0300

    i have changed wellcome to wellcome nafiseh

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,23 +8,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home : Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            image: const DecorationImage(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Container(
+            decoration: const BoxDecoration(
+                image: const DecorationImage(
               image: AssetImage('assets/img/back.jpg'),
               fit: BoxFit.cover,
-            )
-          ),
-          child: const Center(
-            child: Text(
-              'wellcome...',
-              style: TextStyle(color: Colors.white, fontSize: 24),
+            )),
+            child: const Center(
+              child: Text(
+                'wellcome Nafiseh ...',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
             ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
