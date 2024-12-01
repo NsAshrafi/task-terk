40ad6074d922509cc8f11194ee80091eeb708799
Nafiseh Ashrafi <nafisehashrafi@192.168.1.65>
2024-12-01 15:50:32 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home : Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage('assets/img/back.jpg'),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: const Center(
+            child: Text(
+              'wellcome...',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
