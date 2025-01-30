e128ced125ceea492c4df4577c8a04811ee230ab
Nafiseh Ashrafi <nafisehashrafi@Nafisehs-MacBook-Air.local>
2025-01-30 15:36:32 +0300

    added some profile pic on some box

--- lib/pages/set_profilephoto.dart
+++ lib/pages/set_profilephoto.dart
@@ -16,13 +16,50 @@ class SetProfilePhotoPage extends StatelessWidget {
       'assets/img/profiles/8.jpg',
       'assets/img/profiles/9.jpg',
     ];
-    return Scaffold (
-      appBar:AppBar(
-        title: const Text('If You Were An Animal'),
-         backgroundColor: Colors.blueAccent,
-      ) ,
-      
-      );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Profile Photo'),
+        // backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img/backsign.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, 
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: imagePaths.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context, imagePaths[index]); 
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15), 
+                      border: Border.all(color: Colors.blue, width: 2),
+                      image: DecorationImage(
+                        image: AssetImage(imagePaths[index]),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 
