7fa8dad189564db06bb26156aa4f50e6ea5685e8
Nafiseh Ashrafi <nafisehashrafi@Nafisehs-MacBook-Air.local>
2025-01-30 15:52:28 +0300

    added if to profile page icon for updating the photo

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:task_terk/pages/set_profilephoto.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  String? selectedImage;
    @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,30 +24,52 @@ class ProfilePage extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Container(
-                height: 145,
-                width: 145,
-                decoration: BoxDecoration(
-                  // image: const DecorationImage(
-                  //   image: AssetImage('assets/img/logomainn.png'),
-                  //   fit: BoxFit.cover,
-                  // ),
-                  borderRadius: BorderRadius.circular(70.5),
-                  border: Border.all(
-                    width: 1,
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                  ),
-                ),
-                child: const Icon(Icons.person, size: 80, color: Colors.grey),
-              ),
-              const SizedBox(height: 10),
-           
-            GestureDetector(
-                onTap: () {
-                  Navigator.push(
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  final result = await Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => const SetProfilePhotoPage()),
                   );
+                  if (result != null) {
+                    setState(() {
+                      selectedImage = result;
+                    });
+                  }
+                },
+                child: Container(
+                  height: 145,
+                  width: 145,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(70.5),
+                    border: Border.all(
+                      width: 1,
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    image: selectedImage != null
+                        ? DecorationImage(
+                            image: AssetImage(selectedImage!),
+                            fit: BoxFit.cover,
+                          )
+                        : null,
+                  ),
+                  child: selectedImage == null
+                      ? const Icon(Icons.person, size: 80, color: Colors.grey)
+                      : null,
+                ),
+              ),
+              const SizedBox(height: 10),
+              GestureDetector(
+                onTap: () async {
+                  final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SetProfilePhotoPage()),
+                  );
+                  if (result != null) {
+                    setState(() {
+                      selectedImage = result;
+                    });
+                  }
                 },
                 child: const Text(
                   'Set Profile Photo',
@@ -71,17 +100,6 @@ class ProfilePage extends StatelessWidget {
                 ),
               ),
                const SizedBox(height: 20),
-              // TextField(
-              //   decoration: InputDecoration(
-              //     labelText: 'Confirm Password',
-              //     fillColor: Colors.white,
-              //     filled: true,
-              //     border: OutlineInputBorder(
-              //       borderRadius: BorderRadius.circular(10),
-              //     ),
-              //   ),
-              // ),
-              // const SizedBox(height: 50),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -94,13 +112,7 @@ class ProfilePage extends StatelessWidget {
                       ),
                       backgroundColor: Colors.blueAccent,
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ProfilePage()),
-                      );
-                    },
+                    onPressed: () {},
                     child: const Text(
                       'Save',
                       style: TextStyle(
@@ -112,7 +124,6 @@ class ProfilePage extends StatelessWidget {
                   ),
                 ],
               ),
-              
             ],
           ),
         ),
@@ -120,80 +131,3 @@ class ProfilePage extends StatelessWidget {
     );
   }
 }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Profile'),
-//         backgroundColor: Colors.blueAccent,
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-            
-//             Container(
-//               width: 150,
-//               height: 150,
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(75),
-//                 border: Border.all(color: Colors.grey, width: 2),
-//               ),
-//               child: const Icon(Icons.person, size: 80, color: Colors.grey),
-//             ),
-//             const SizedBox(height: 10),
-           
-//             const Text(
-//               'Set Profile Photo',
-//               style: TextStyle(fontSize: 18, color: Colors.blueAccent),
-//             ),
-//             const SizedBox(height: 20),
-            
-//             TextField(
-//               decoration: InputDecoration(
-//                 labelText: 'Name',
-//                 border: OutlineInputBorder(
-//                   borderRadius: BorderRadius.circular(10),
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 20),
-           
-//             TextField(
-//               decoration: InputDecoration(
-//                 labelText: 'Username',
-//                 border: OutlineInputBorder(
-//                   borderRadius: BorderRadius.circular(10),
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 30), 
-            
-//             ElevatedButton(
-//               style: ElevatedButton.styleFrom(
-//                 padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 25),
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(20),
-//                 ),
-//                 backgroundColor: Colors.blueAccent,
-//               ),
-//               onPressed: () {
-                
-//                 ScaffoldMessenger.of(context).showSnackBar(
-//                   const SnackBar(content: Text('Start Button Pressed!')),
-//                 );
-//               },
-//               child: const Text(
-//                 'Start',
-//                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
